2fd24c821fc9e58acf8c1e702aa283fa641b1b7d
skypedanny <skypedanny@naver.com>
2020-12-26 16:09:19 +0900

    [CREATE] Create Start-Page

---
+++ src/pages/start/Style.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const Positioner = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Logo = styled.div`
+  margin-left: 8%;
+  margin-top: 8%;
+  margin-right: auto;
+`;
+
+export const Desc = styled.div`
+  margin-left: auto;
+  margin-right: 4%;
+  margin-top: 1%;
+
+  font-family: Apple SD Gothic Neo;
+  font-style: normal;
+  font-weight: 100;
+  font-size: 2.7vw;
+  line-height: 150%;
+  text-align: right;
+
+  color: #000000;
+
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Wrapper = styled.div`
+  width: 80%;
+  margin-top: 6%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Emotion = styled.div`
+  margin-top: 10%;
+  display: flex;
+`;
+
+export const Sign = styled.div`
+  cursor: pointer;
+  margin-top: auto;
+  margin-bottom: 1%;
+  font-family: Apple SD Gothic Neo;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 2vw;
+  line-height: 130%;
+
+  text-align: right;
+
+  color: #000000;
+`;
