a99548f0c76f27a15e14cd3709113708b805b92f
dann1y <skypedanny@naver.com>
2021-01-08 23:03:12 +0900

    [CRAETE] Publish Content

---
+++ src/Components/Content/Content.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Style";
+
+const Content: React.FC = () => {
+  return (
+    <S.Positioner>
+      <S.Wrapper>wassup content</S.Wrapper>
+    </S.Positioner>
+  );
+};
+
+export default Content;
---
+++ src/Components/Content/Style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Positioner = styled.div`
+  background: #f2f2f2;
+  box-shadow: inset 0px 0px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 50px 50px 0px 0px;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
