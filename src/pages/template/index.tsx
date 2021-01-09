e1a451e59ca7f9db28c7bfaaabdb423328cd6ba8
dann1y <skypedanny@naver.com>
2021-01-09 23:35:03 +0900

    [UPDATE] Update Page Template

--- src/pages/template/index.tsx
+++ src/pages/template/index.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Content from "src/Components/Content/Content";
 import Header from "src/Components/Header/Header";
 import LeftMenu from "src/Components/LeftMenu/LeftMenu";
+import RightMenu from "src/Components/RightMenu/RightMenu";
 import * as S from "./Style";
+
+const titleList = [
+  {
+    router: "home",
+    title: "홈",
+  },
+  {
+    router: "mystory",
+    title: "나의 이야기",
+  },
+  {
+    router: "ourstory",
+    title: "우리의 이야기",
+  },
+  {
+    router: "nletter",
+    title: "N일 뒤의 편지",
+  },
+  {
+    router: "community",
+    title: "소통하기",
+  },
+];
 
 const index: React.FC = () => {
   return (
@@ -13,8 +38,12 @@ const index: React.FC = () => {
         <S.LeftMenu>
           <LeftMenu />
         </S.LeftMenu>
-        <S.Content></S.Content>
-        <S.RightMenu></S.RightMenu>
+        <S.Content>
+          <Content />
+        </S.Content>
+        <S.RightMenu>
+          <RightMenu />
+        </S.RightMenu>
       </S.Wrapper>
     </S.Positioner>
   );
