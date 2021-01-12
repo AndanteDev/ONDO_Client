164384359a5dcf7360cf39c39d73d564f312108e
dann1y <skypedanny@naver.com>
2021-01-12 22:37:17 +0900

    [CREATE] Publish DiaryPhoto

---
+++ src/Components/DiaryPhoto/Style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Positioner = styled.div`
+  width: 15vw;
+  height: 40vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: #ffffff;
+`;
+
+export const Photo = styled.div`
+  width: 90%;
+  height: 80%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2vh;
+
+  background-color: #d8d8d8;
+
+  cursor: pointer;
+`;
