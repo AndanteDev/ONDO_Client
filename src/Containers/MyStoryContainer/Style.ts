65d7bd6850a667214384ede0e3b5e43e9584ce83
dann1y <skypedanny@naver.com>
2021-01-12 22:35:08 +0900

    [CREATE] Create MyStoryContainer

---
+++ src/Containers/MyStoryContainer/Style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Positioner = styled.div`
+  width: 94%;
+  margin-bottom: 30vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  justify-items: center;
+`;
